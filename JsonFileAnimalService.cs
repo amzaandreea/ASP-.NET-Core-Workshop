276bc2d13defa73c7209141b6c74011b16869c0f
amzaandreea <47570739+amzaandreea@users.noreply.github.com>
2020-09-02 11:36:58 +0300

    Add files via upload

---
+++ JsonFileAnimalService.cs
@@ -0,0 +1,33 @@
+﻿using FeedIt.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace FeedItv2.Services
+{
+    public class JsonFileAnimalService
+    {
+        public JsonFileAnimalService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "animals.json"); }
+        }
+
+        public IEnumerable<Animal> GetAnimals()
+        {
+            using StreamReader jsonFileReader = File.OpenText(JsonFileName);
+
+            return JsonSerializer.Deserialize<Animal[]>(jsonFileReader.ReadToEnd());
+        }
+
+        
+    }
+}
